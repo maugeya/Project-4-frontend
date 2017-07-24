1409ae299357998a9750a9ab286fc14a370421aa
Jack11709 <jackmay89@live.com>
2017-07-24 12:07:23 +0100

    facebook oauth

--- src/js/config/satellizer.js
+++ src/js/config/satellizer.js
@@ -1,11 +1,17 @@
 angular
-  .module('spotlightApp')
-  .config(Auth);
+.module('spotlightApp')
+.config(Auth);
 
 
 Auth.$inject = ['$authProvider', 'API_URL'];
 function Auth($authProvider, API_URL) {
   $authProvider.signupUrl = `${API_URL}/register`;
   $authProvider.loginUrl = `${API_URL}/login`;
+
+
+  $authProvider.facebook({
+    clientId: '482550242088565',
+    url: `${API_URL}/oauth/facebook`
+  });
 
 }
